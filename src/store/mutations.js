0e32761f076a47315419c68fee126f9af6e27cc7
wangshaojie <wsj110@aliyun.com>
2019-04-15 23:02:01 +0800

    init state of player page

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,8 +1,27 @@
 import * as types from './mutation-types'
+import { type } from 'os'
 
 const matutaions = {
   [types.SET_SINGER](state, singer) {
     state.singer = singer
+  },
+  [types.SET_PLAYING_STATE](state, flag) {
+    state.playing = flag
+  },
+  [types.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag
+  },
+  [type.SET_PLAYING_STATE](state, list) {
+    state.playList = list
+  },
+  [type.SET_SEQUENCE_LIST](state, list) {
+    state.sequenceList = list
+  },
+  [type.SET_PLAY_MODE](state, list) {
+    state.mode = list
+  },
+  [type.SET_CURRENT_INDEX](state, index) {
+    this.currentIndex = index
   }
 }
 
--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,11 @@
+import { playMode } from 'common/js/config'
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playList: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1 // 当前播放
 }
 export default state
